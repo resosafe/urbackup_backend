ff235c8193257cb26793711d0de47def60c14735
Martin <martin@urbackup.org>
2024-05-07 20:30:07 +0200

    Format with prettier and add lingui

--- urbackupserver/www2/src/App.tsx
+++ urbackupserver/www2/src/App.tsx
@@ -1,57 +1,61 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import HeaderBar from './components/HeaderBar';
-import NavSidebar from './components/NavSidebar';
-import { proxy, useSnapshot } from 'valtio';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
-import LoginPage from './pages/Login';
-import StatusPage from './pages/Status';
-import { FluentProvider, teamsLightTheme, teamsDarkTheme, makeStyles } from '@fluentui/react-components';
-import { useStackStyles } from './components/StackStyles';
-import UrBackupServer from './api/urbackupserver';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import * as React from "react";
+import { Suspense, useEffect, useState } from "react";
+import HeaderBar from "./components/HeaderBar";
+import NavSidebar from "./components/NavSidebar";
+import { proxy, useSnapshot } from "valtio";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import StatusPage from "./pages/Status";
+import {
+  FluentProvider,
+  teamsLightTheme,
+  teamsDarkTheme,
+  makeStyles,
+  Spinner,
+} from "@fluentui/react-components";
+import { useStackStyles } from "./components/StackStyles";
+import UrBackupServer from "./api/urbackupserver";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
 
-const initialDark = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-const initialTheme =
-  initialDark ? teamsDarkTheme : teamsLightTheme;
+const initialDark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+const initialTheme = initialDark ? teamsDarkTheme : teamsLightTheme;
 
 export enum Pages {
   Status = "status",
   Activities = "activities",
   Login = "login",
-  About = "about"
+  About = "about",
 }
 
 export const state = proxy({
   loggedIn: false,
-  activePage: Pages.Status
+  activePage: Pages.Status,
 });
-
 
 export const router = createHashRouter([
   {
     path: "/",
-    element: (
-      <LoginPage />
-    ),
+    element: <LoginPage />,
     loader: async () => {
       state.activePage = Pages.Login;
       return null;
-    }
+    },
   },
   {
     path: `/${Pages.Status}`,
-    element: (
-      <StatusPage />
-    ),
+    element: <StatusPage />,
     loader: async () => {
       state.activePage = Pages.Status;
       return null;
-    }
+    },
   },
   {
     path: "/about",
-    element: <div>About page</div>
+    element: <div>About page</div>,
   },
   {
     path: `/${Pages.Activities}`,
@@ -59,12 +63,19 @@ export const router = createHashRouter([
     loader: async () => {
       state.activePage = Pages.Activities;
       return null;
-    }
-  }
+    },
+  },
 ]);
 
 export const urbackupServer = new UrBackupServer("x");
 const queryClient = new QueryClient();
+
+export async function dynamicActivateTranslation(locale: string) {
+  const { messages } = await import(`./locales/${locale}.po`);
+
+  i18n.load(locale, messages);
+  i18n.activate(locale);
+}
 
 const App: React.FunctionComponent = () => {
   const [selectedTheme, setTheme] = useState(initialTheme);
@@ -72,37 +83,47 @@ const App: React.FunctionComponent = () => {
   const snap = useSnapshot(state);
 
   useEffect(() => {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-      setTheme(event.matches ? teamsDarkTheme : teamsLightTheme);
-    });
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        setTheme(event.matches ? teamsDarkTheme : teamsLightTheme);
+      });
+    dynamicActivateTranslation("en");
   }, []);
 
   const styles = useStackStyles();
 
   return (
-      <FluentProvider theme={selectedTheme} style={{ height: "100%" }}>
-        <React.StrictMode>
+    <FluentProvider theme={selectedTheme} style={{ height: "100%" }}>
+      <React.StrictMode>
+        <I18nProvider i18n={i18n}>
           <QueryClientProvider client={queryClient}>
             <div className={styles.stackVertical}>
               <div className={styles.item}>
-                  <HeaderBar />
-                </div>
-                <div className={styles.itemGrow}>
-                  <div className={styles.stackHorizontal}>
-                    {snap.loggedIn &&
-                      <div className={styles.item} style={{ borderRight: "1px solid", padding: "10pt" }}>
-                        <NavSidebar />
-                      </div>
-                    }
-                    <div className={styles.itemGrow} style={{padding: "10pt"}}>
-                      <RouterProvider router={router} />
+                <HeaderBar />
+              </div>
+              <div className={styles.itemGrow}>
+                <div className={styles.stackHorizontal}>
+                  {snap.loggedIn && (
+                    <div
+                      className={styles.item}
+                      style={{ borderRight: "1px solid", padding: "10pt" }}
+                    >
+                      <NavSidebar />
                     </div>
+                  )}
+                  <div className={styles.itemGrow} style={{ padding: "10pt" }}>
+                    <Suspense fallback={<Spinner />}>
+                      <RouterProvider router={router} />
+                    </Suspense>
+                  </div>
                 </div>
               </div>
             </div>
           </QueryClientProvider>
-        </React.StrictMode>
-      </FluentProvider>
+        </I18nProvider>
+      </React.StrictMode>
+    </FluentProvider>
   );
 };
 
--- urbackupserver/www2/src/components/StackStyles.tsx
+++ urbackupserver/www2/src/components/StackStyles.tsx
@@ -1,49 +1,49 @@
-import {makeStyles } from '@fluentui/react-components';
+import { makeStyles } from "@fluentui/react-components";
 
 export const useStackStyles = makeStyles({
-    stackVertical: {
-      display: 'flex',
-      flexDirection: 'column',
-      flexWrap: 'nowrap',
-      width: 'auto',
-      height: '100%',
-      boxSizing: 'border-box',
-      '> *': {
-        textOverflow: 'ellipsis',
-      },
-      '> :not(:first-child)': {
-        marginTop: '0px',
-      },
-      '> *:not(.ms-StackItem)': {
-        flexShrink: 1,
-      },
+  stackVertical: {
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "nowrap",
+    width: "auto",
+    height: "100%",
+    boxSizing: "border-box",
+    "> *": {
+      textOverflow: "ellipsis",
     },
-    stackHorizontal: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'nowrap',
-      width: 'auto',
-      height: "100%",
-      boxSizing: 'border-box',
-      '> *': {
-        textOverflow: 'ellipsis',
-      },
-      '> :not(:first-child)': {
-        marginTop: '0px',
-      },
-      '> *:not(.ms-StackItem)': {
-        flexShrink: 1,
-      },
+    "> :not(:first-child)": {
+      marginTop: "0px",
     },
-    item: {
-      height: 'auto',
-      width: 'auto',
+    "> *:not(.ms-StackItem)": {
       flexShrink: 1,
     },
-    itemGrow: {
-      height: 'auto',
-      width: 'auto',
-      flexShrink: 1,
-      flexGrow: 1
+  },
+  stackHorizontal: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    width: "auto",
+    height: "100%",
+    boxSizing: "border-box",
+    "> *": {
+      textOverflow: "ellipsis",
     },
-  });
+    "> :not(:first-child)": {
+      marginTop: "0px",
+    },
+    "> *:not(.ms-StackItem)": {
+      flexShrink: 1,
+    },
+  },
+  item: {
+    height: "auto",
+    width: "auto",
+    flexShrink: 1,
+  },
+  itemGrow: {
+    height: "auto",
+    width: "auto",
+    flexShrink: 1,
+    flexGrow: 1,
+  },
+});

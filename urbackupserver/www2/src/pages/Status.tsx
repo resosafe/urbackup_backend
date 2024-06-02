427154f201acaa3e98973f1e9b551a65e3644d8e
Kazi <kazinoshin.jo@gmail.com>
2024-06-02 14:58:06 +0600

    Add pagination

--- urbackupserver/www2/src/pages/Status.tsx
+++ urbackupserver/www2/src/pages/Status.tsx
@@ -6,14 +6,36 @@ import {
   DataGridHeader,
   DataGridHeaderCell,
   DataGridRow,
+  makeStyles,
+  Select,
   Spinner,
   TableCellLayout,
   TableColumnDefinition,
+  tokens,
 } from "@fluentui/react-components";
-import { Suspense} from "react";
-import { useQuery } from "react-query";
 import { StatusClientItem } from "../api/urbackupserver";
+import { Suspense, useState } from "react";
+import { useQuery } from "react-query";
+import { Pagination } from "@fluentui/react-experiments";
 import { urbackupServer } from "../App";
+import { chunk } from "../utils/chunk";
+import { registerIcons } from "@fluentui/react-experiments/lib/Styling";
+import {
+  ArrowNext20Regular,
+  ArrowPrevious20Regular,
+  ChevronLeft20Regular,
+  ChevronRight20Regular,
+} from "@fluentui/react-icons";
+
+// Register icons used in Pagination @fluentui/react-experiments. See https://github.com/microsoft/fluentui/wiki/Using-icons#registering-custom-icons.
+registerIcons({
+  icons: {
+    CaretSolidLeft: <ChevronLeft20Regular />,
+    CaretSolidRight: <ChevronRight20Regular />,
+    Next: <ArrowNext20Regular />,
+    Previous: <ArrowPrevious20Regular />,
+  },
+});
 
 const compareNum = (a: number, b: number) => {
   return a == b ? 0 : a < b ? 1 : -1;
@@ -78,21 +100,63 @@ const columns: TableColumnDefinition<StatusClientItem>[] = [
   }),
 ];
 
+const useStyles = makeStyles({
+  pageSize: {
+    display: "flex",
+    alignItems: "center",
+    gap: tokens.spacingHorizontalS,
+  },
+  dataGrid: {
+    marginBlockStart: tokens.spacingHorizontalM,
+  },
+});
+
+const paginationStyles = {
+  pageNumber: {
+    verticalAlign: "top",
+  },
+};
+
+const DEFAULT_PAGE_SIZE = 25;
+const PAGE_SIZES = [10, 25, 50, 100];
+
 const Status = () => {
   const statusResult = useQuery("status", urbackupServer.status, {
     suspense: true,
   });
 
+  const [pageSize, setPageSize] = useState<number>(DEFAULT_PAGE_SIZE);
+  const [page, setPage] = useState(0);
+
+  const classes = useStyles();
+
+  const dataItems = statusResult.data!.status;
+  const pageData = chunk(dataItems, pageSize);
+
   return (
     <>
       <Suspense fallback={<Spinner />}>
         <h3>Status page</h3>
+        <label className={classes.pageSize}>
+          Show
+          <Select
+            id="page-size"
+            defaultValue={pageSize}
+            onChange={(_, data) => setPageSize(+data.value)}
+          >
+            {PAGE_SIZES.map((size) => (
+              <option>{size}</option>
+            ))}
+          </Select>
+          entries
+        </label>
         <DataGrid
           sortable
           selectionMode="multiselect"
-          items={statusResult.data!.status}
+          items={pageData[page]}
           getRowId={(item) => item.id}
           columns={columns}
+          className={classes.dataGrid}
         >
           <DataGridHeader>
             <DataGridRow selectionCell={{ "aria-label": "Select all rows" }}>
@@ -114,6 +178,23 @@ const Status = () => {
             )}
           </DataGridBody>
         </DataGrid>
+        <div>
+          <Pagination
+            selectedPageIndex={page}
+            pageCount={pageData.length}
+            itemsPerPage={pageSize}
+            totalItemCount={dataItems.length}
+            format={"buttons"}
+            previousPageAriaLabel={"previous page"}
+            nextPageAriaLabel={"next page"}
+            firstPageAriaLabel={"first page"}
+            lastPageAriaLabel={"last page"}
+            pageAriaLabel={"page"}
+            selectedAriaLabel={"selected"}
+            onPageChange={(index) => setPage(index)}
+            styles={paginationStyles}
+          />
+        </div>
       </Suspense>
     </>
   );

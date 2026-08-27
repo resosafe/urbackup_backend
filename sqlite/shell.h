7a27ed84b7cb06f8dfad1cc20099b14710608c03
Jean Collonvillé <jcollonville@resosafe.fr>
2026-08-27 10:43:49 +0200

    fix merge

--- sqlite/shell.h
+++ sqlite/shell.h
@@ -3,49 +3,7 @@
 struct ShellState;
 ShellState* shell_state_init(const char* db_fn, FILE* out);
 
-<<<<<<< HEAD
-typedef struct ExpertInfo ExpertInfo;
-struct ExpertInfo {
-    sqlite3expert* pExpert;
-    int bVerbose;
-};
-
-/* A single line in the EQP output */
-typedef struct EQPGraphRow EQPGraphRow;
-struct EQPGraphRow {
-    int iEqpId;           /* ID for this row */
-    int iParentId;        /* ID of the parent row */
-    EQPGraphRow* pNext;   /* Next row in sequence */
-    char zText[1];        /* Text to display for this row */
-};
-
-/* All EQP output is collected into an instance of the following */
-typedef struct EQPGraph EQPGraph;
-struct EQPGraph {
-    EQPGraphRow* pRow;    /* Linked list of all rows of the EQP output */
-    EQPGraphRow* pLast;   /* Last element of the pRow list */
-    char zPrefix[100];    /* Graph prefix */
-};
-
-#if defined(SQLITE_ENABLE_SESSION)
-typedef struct OpenSession OpenSession;
-struct OpenSession {
-    char *zName;
-    int nFilter;
-    char **azFilter;
-    sqlite3_session *p;
-};
-#endif
-
-typedef struct ColModeOpts ColModeOpts;
-struct ColModeOpts {
-    int iWrap;
-    u8 bQuote;
-    u8 bWordWrap;
-};
-=======
 void shell_state_free(ShellState* state);
->>>>>>> upstream/2.5.x
 
 typedef struct ShellState ShellState;
 struct ShellState {
